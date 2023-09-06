24d366fd6746c1b66d198a35a9a6ccd5e61a998e
Desi DeVaul <ddevaul@princeton.edu>
2023-09-06 03:16:41 -0400

    fixed another linter error

--- src/pages/psellos/#[id].tsx
+++ src/pages/psellos/#[id].tsx
@@ -1,30 +1,30 @@
-import { useRouter } from 'next/router';
-import { withPageAuthRequired } from '@auth0/nextjs-auth0';
-import Head from 'next/head';
-import Header from 'components/header';
-import styles from '../../styles/Analyze-Psellos.module.css'
-import { useText } from '../api/services/use-text';
+// import { useRouter } from 'next/router';
+// import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+// import Head from 'next/head';
+// import Header from 'components/header';
+// import styles from '../../styles/Analyze-Psellos.module.css'
+// import { useText } from '../api/services/use-text';
  
-export default function Page() {
-  const router = useRouter();
-  const { text } = useText({
+// export default function Page() {
+//   const router = useRouter();
+//   const { text } = useText({
 
-    url: `/api/data/text`,
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    data: {
-      text_id: router.query.id
-    }
-  });
+//     url: `/api/data/text`,
+//     method: "POST",
+//     headers: {
+//       "content-type": "application/json",
+//     },
+//     data: {
+//       text_id: router.query.id
+//     }
+//   });
 
-  return (
-    <div>
-      <p>Post: {router.query.id}</p>
-    </div>
-  )
+//   return (
+//     <div>
+//       <p>Post: {router.query.id}</p>
+//     </div>
+//   )
 
-}
+// }
 
-export const getServerSideProps = withPageAuthRequired();
+// export const getServerSideProps = withPageAuthRequired();
