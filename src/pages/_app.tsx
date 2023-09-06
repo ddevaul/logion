5cde1e4694e6f01de503d99085d69d730be251b5
Desi DeVaul <ddevaul@princeton.edu>
2023-09-06 05:06:23 -0400

    asdf

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,9 @@ import { type AppType } from "next/dist/shared/lib/utils";
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import "~/styles/globals.css";
 
+
+
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <UserProvider>
